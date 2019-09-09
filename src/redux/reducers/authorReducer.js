1a172d5ed0019ded446c0af9dc34ea0282f82cda
devibrahem <dev.ibrahembastawi@gmail.com>
2019-09-09 15:42:45 +0200

    handling errors and aysnc actions

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,5 +1,6 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
+
 export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
     case types.LOAD_AUTHORS_SUCCESS:
